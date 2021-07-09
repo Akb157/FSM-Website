d5f843bccb761407407fb43dd1833e53c7dbba83
Akb157 <akbaheti157@gmail.com>
2021-07-09 22:51:20 +0530

    authentication updated

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -1,5 +1,3 @@
-
-
 // Auth
 auth.onAuthStateChanged(user => {
     if (user) {
@@ -8,13 +6,13 @@ auth.onAuthStateChanged(user => {
       document.getElementById('sin').style.display = "none";
       document.getElementById('logouts').style.display = "inline-block";
       console.log(user.email + " is logged in!");
-      alert(user.email + " is logged in!");
+      alert(user.email + "  is logged in!");
     } else {
       
       document.getElementById('lin').style.display = "inline-block";
       document.getElementById('sin').style.display = "inline-block";
       document.getElementById('logouts').style.display = "none";
       console.log('User is logged out!');
-      alert(user.email + 'User is logged out!');
+      alert(user.email + '  is logged out!');
     }
   });
